149b7113ee97d89331b5c44ca9644c93ada12165
Sasha Moak <sasha.moak@gmail.com>
2020-07-31 13:36:00 -0700

    Enable concurrent mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// necessary because getElementById returns HTMLElement | null
+// but createRoot does not accept null as a valid value so TS complains
+const root = document.getElementById('root') || document.createElement('div');
+
+ReactDOM.unstable_createRoot(root).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
