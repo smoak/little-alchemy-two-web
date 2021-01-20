bdafcf6000761cdf13b90d4fea6195897bb0265e
Sasha Moak <sasha.moak@gmail.com>
2021-01-20 15:02:52 -0800

    feat(header): Adding home button support
    Allowing the home button in the header to be clicked on and take you
    back to the home (search) page.

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -1,14 +1,22 @@
 import { Box, Button, Header, Heading } from 'grommet';
 import { Home } from 'grommet-icons';
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export const AppHeader: FC = () => (
-  <Header background="brand">
-    <Button icon={<Home />} hoverIndicator />
-    <Box fill align="center" justify="start">
-      <Heading level="3" margin="none">
-        Little Alchemy 2
-      </Heading>
-    </Box>
-  </Header>
-);
+export const AppHeader: FC = () => {
+  const history = useHistory();
+  const homeButtonClickCallback = useCallback(() => {
+    history.push('/');
+  }, [history]);
+
+  return (
+    <Header background="brand">
+      <Button icon={<Home />} hoverIndicator onClick={homeButtonClickCallback} />
+      <Box fill align="center" justify="start">
+        <Heading level="3" margin="none">
+          Little Alchemy 2
+        </Heading>
+      </Box>
+    </Header>
+  );
+};
